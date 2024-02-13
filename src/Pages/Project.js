5d59ad8a82b702b891d74cb5ddcf2accfd915004
Hisano Sato <hisano@Hisanos-MacBook-Air.local>
2024-02-13 22:41:31 +1000

    add project

--- src/Pages/Project.js
+++ src/Pages/Project.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "../Styles/Project.css";
 import comingSoon from "../../src/assets/comingSoon.png";
-import { Row, Typography, Col } from "antd";
+import weather from "../../src/assets/weather.png";
+import weatherScreen from "../../src/assets/weatherScreen.png";
+import { Row, Typography, Col, Button, Modal } from "antd";
 
 const { Title } = Typography;
 
 function Project() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
   return (
     <div className="projectContainer">
       <div style={{ paddingTop: "80px" }}>
@@ -27,7 +39,72 @@ function Project() {
       </Col>
       <Row justify={"center"}>
         <Col xs={22} sm={18} md={12} xl={8}>
-          <img className="projectImage" src={comingSoon} alt="Sample" />
+          <div className="projectItem">
+            <img className="projectImage" src={weather} alt="weather-project" />
+            <div class="middle">
+              <Title
+                style={{
+                  textAlign: "center",
+                  color: "#ffffff",
+                  lineHeight: "1.9",
+                  fontSize: "18px",
+                }}
+                level={3}
+              >
+                Weather Forecast
+              </Title>
+              <p className="text">
+                It is a platform which allows you to check weather forecast in
+                your place!
+              </p>
+
+              <Button
+                onClick={showModal}
+                style={{
+                  color: "#ffffff",
+                  fontSize: "17px",
+                  borderColor: "#3bcbab",
+                  backgroundColor: "#3bcbab",
+                }}
+                backgroundColor="#2CD4D9"
+                size="large"
+              >
+                Read more
+              </Button>
+              <Modal
+                title="Weather Forecast"
+                footer={null}
+                open={isModalOpen}
+                onOk={handleOk}
+                onCancel={handleCancel}
+                width={1000}
+              >
+                <img
+                  className="projectScreen"
+                  src={weatherScreen}
+                  alt="weather-project"
+                />
+                <div>
+                  <p>description</p>
+                </div>
+                <div>
+                  <Button
+                    style={{
+                      color: "#ffffff",
+                      fontSize: "17px",
+                      borderColor: "#3bcbab",
+                      backgroundColor: "#3bcbab",
+                    }}
+                    href="https://check-weather123.netlify.app/"
+                    backgroundColor="#2CD4D9"
+                    size="large"
+                  >
+                    Visit site
+                  </Button>
+                </div>
+              </Modal>
+            </div>
+          </div>
         </Col>
         <Col xs={22} sm={18} md={12} xl={8}>
           <img className="projectImage" src={comingSoon} alt="Sample" />
